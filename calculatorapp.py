9d391f10a66c126f2dbb42aa5aecddf658d37ba5
Isabela Ferreira Paranaíba <isafparanaiba@gmail.com>
2022-11-01 18:34:02 -0300

    Update calculatorapp.py

--- calculatorapp.py
+++ calculatorapp.py
@@ -1,9 +1,19 @@
 import streamlit
 import pandas
-import hashlib 
+import hashlib
+import statistics
 from PIL import Image
 from io import BytesIO
 import requests 
 
 image = Image.open('Calculator.png') 
+
+
+#list of positive integer numbers
+data1 = [1, 3, 4, 5, 7, 9, 2]
+
+x = statistics.mean(data1)
+
+#printing the mean
+print("Mean is :", x)
  
