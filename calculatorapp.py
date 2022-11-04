14a79e6dc79c9090b1a3f3279ed36f9121552e22
Isabela Ferreira Paranaíba <isafparanaiba@gmail.com>
2022-11-04 14:04:59 -0300

    Update calculatorapp.py

--- calculatorapp.py
+++ calculatorapp.py
@@ -16,18 +16,18 @@ def mul(a, b):
 def div(a, b):
   return a / b
 def main():
-st.title("Simple Calculator")
-st.write("This is a simple calculator app")
-a = st.number_input("Enter a number")
-b = st.number_input("Enter another number")
-operation = st.selectbox("Select Operation", ["Add", "Subtract", "Multiply", "Divide"])
-if operation == "Add":
-st.write(add(a, b))
-elif operation == "Subtract":
-st.write(sub(a, b))
-elif operation == "Multiply":
-st.write(mul(a, b))
-elif operation == "Divide":
-st.write(div(a, b))
+  st.title("Simple Calculator")
+  st.write("This is a simple calculator app")
+  a = st.number_input("Enter a number")
+  b = st.number_input("Enter another number")
+  operation = st.selectbox("Select Operation", ["Add", "Subtract", "Multiply", "Divide"])
+  if operation == "Add":
+  st.write(add(a, b))
+  elif operation == "Subtract":
+  st.write(sub(a, b))
+  elif operation == "Multiply":
+  st.write(mul(a, b))
+  elif operation == "Divide":
+  st.write(div(a, b))
 if __name__ == '__main__':
 main()
