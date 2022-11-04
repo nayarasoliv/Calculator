8cbbfb47e5511cae2e16ca56c6c0fdd04dc7ff7b
Isabela Ferreira Paranaíba <isafparanaiba@gmail.com>
2022-11-04 14:04:12 -0300

    Update calculatorapp.py

--- calculatorapp.py
+++ calculatorapp.py
@@ -7,38 +7,27 @@ import streamlit as st
 #import requests 
 #import main
 
-# st.title("") para TÍTULO
-st.title ("Calculator")
-#image = Image.open('Calculator.png') 
-vetor = []
-
- #coletando dados
-q = st.number_input("Quantos números serão informados?")
-n1 = st.number_input("Número 1:")
-n2 = st.number_input("Número 2:")
-n3 = st.number_input("Número 3:")
-n4 = st.number_input("Número 4:")
-n5 = st.number_input("Número 5:")
-
-operation = st.selectbox("Select Operation", ["Add", "Subtract", "Multiply", "Média"])
-
-if st.button('ENTER'):
-    vetor.append(n1)
-else:
-    st.write('ENTER')
-
-tamanho = len(vetor)
-for i in range(tamanho):
-    st.info(vetor[i])
-
-  
+def add(a, b):
+  return a + b
+def sub(a, b):
+  return a - b
+def mul(a, b):
+  return a * b
+def div(a, b):
+  return a / b
+def main():
+st.title("Simple Calculator")
+st.write("This is a simple calculator app")
+a = st.number_input("Enter a number")
+b = st.number_input("Enter another number")
+operation = st.selectbox("Select Operation", ["Add", "Subtract", "Multiply", "Divide"])
 if operation == "Add":
-    st.write(str(n1 + n2 + n3 + n4 + n5))
+st.write(add(a, b))
 elif operation == "Subtract":
-    st.write(str(n1 - n2 - n3 - n4 - n5))
+st.write(sub(a, b))
 elif operation == "Multiply":
-    st.write(str(n1 * n2 * n3 * n4 * n5))
-elif operation == "Média":
-    st.write (str(n1 + n2 + n3 + n4 + n5)/(q))
-else:
-    st.write ("Selecione")
+st.write(mul(a, b))
+elif operation == "Divide":
+st.write(div(a, b))
+if __name__ == '__main__':
+main()
